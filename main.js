9298c08c0af86ccde2083d2606bc08827b475d3a
Witek Podskarbi <witdev.front@gmail.com>
2018-12-05 23:49:47 +0100

    feat: added media queries and before element for product attributes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// SWITCH CONTENT //
 const nextPageBtn = document.getElementById('next-page');
 const previousPageBtn = document.getElementById('previous-page');
 const visiblePage = document.getElementById('first-page');
@@ -19,25 +20,34 @@ const togglePreviousContent = (e) => {
 nextPageBtn.addEventListener('click', toggleNextContent);
 previousPageBtn.addEventListener('click', togglePreviousContent);
 
-// THEME SWITCH //
 
+
+// THEME SWITCH //
 const toggleBtn = document.querySelector('.toggle');
 
 const headerTitle = document.querySelector('.header__title');
 const headerSubtitle = document.querySelector('.header__subtitle');
+const btnHeader = document.querySelector('.btn__header');
+const btnNext = document.querySelector('.btn__next');
+const btnNextLable = document.querySelector('.btn__label');
+const productsTitle = document.querySelector('.products__section-title');
+const questionHeading = [...document.querySelectorAll('.product__question-content')];
 const stepperElement = [...document.querySelectorAll('.stepper__element')];
-const questionHeading = [...document.querySelectorAll('.products__question-content')];
 const product = [...document.querySelectorAll('.product__add')];
 
 const switchDarkTheme = function(e) {
     toggleBtn.classList.toggle('active');
     headerTitle.classList.toggle('dark-txt-color');
     headerSubtitle.classList.toggle('dark-txt-color');
+    btnHeader.classList.toggle('dark-bg-color');
+    btnNext.classList.toggle('dark-bg-color');
+    btnNextLable.classList.toggle('dark-txt-color');
+    productsTitle.classList.toggle('dark-bg-color');
+    [...questionHeading].forEach((qHeadingEl) => {
+        qHeadingEl.classList.toggle('dark-txt-color');
+    });
     [...stepperElement].forEach((stepEl) => {
         stepEl.classList.toggle('dark-bg-color');
-    });
-    [...questionHeading].forEach((qhEl) => {
-        qhEl.classList.toggle('dark-bg-color');
     });
     [...product].forEach((product) => {
         product.classList.toggle('dark-bg-color');
